6da9d224084730e23113c13324ab611eeecbb1fb
jkrolling98 <65852623+jkrolling98@users.noreply.github.com>
2023-02-13 23:16:27 +0800

    added skeleton console code for client

--- src/client/Client.java
+++ src/client/Client.java
@@ -42,14 +42,106 @@ public class Client {
         sc.close();
     }
 
+    public static void printMenu(String[] options){
+        System.out.println("\n\nWelcome to SCSE Flight information system");
+        System.out.println("Please select one of the following options by entering the number");
+        for (String option : options){
+            System.out.println(option);
+        }
+        System.out.print("Choose your option : ");
+    }
+
+    public void queryFlights(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("To query for flights based on flight routes, please enter the desired source and destination.");
+        System.out.println("Please enter the desired source location.");
+        try {
+            String src = sc.nextLine();
+            System.out.println("Please enter the desired destination location.");
+            String dst = sc.nextLine();
+            System.out.println(String.format("Here are the flights travelling from %s to %s",src,dst));
+            System.out.println("blahblah");
+        }catch (Exception ex){
+            System.out.println("Error: Input is not an integer.");
+        }
+    }
+
+    public void queryFlightByID(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("To query for flights based on flight ID, please enter the FlightID.");
+        System.out.println("Please enter the FlightID.");
+        try {
+            int flightID = sc.nextInt();
+            System.out.println(String.format("Here are the details about Flight ID: %d",flightID));
+            System.out.println("to be added");
+        }catch (Exception ex){
+            System.out.println("Error: Input is not an integer.");
+        }
+    }
+
+    public void makeReservation(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("To make reservation for flight, please enter the FlightID and the desired amount of seats.");
+        System.out.println("Please enter the FlightID.");
+        try {
+            int flightID = sc.nextInt();
+            System.out.println("Please enter the number of seats you wish to book.");
+            int seatCount = sc.nextInt();
+            System.out.println(String.format("%d seats booked for Flight ID: %d",seatCount,flightID));
+        }catch (Exception ex){
+            System.out.println("Error: Input is not an integer.");
+        }
+    }
+
+    public void runConsole(){
+        String[] options = {
+                "[1] Query flights based on flight routes",
+                "[2] Query flight ID",
+                "[3] Make reservation for flight",
+                "[4] Exit",
+        };
+        Scanner sc = new Scanner(System.in);
+        int userInput = 0;
+        while(userInput!=4){
+            printMenu(options);
+            try {
+                    userInput = sc.nextInt();
+                    System.out.println("You entered: " + userInput);
+                    boolean done = false;
+                    switch (userInput) {
+                        case 1: //invoke flights query
+                            queryFlights();
+                            break;
+                        case 2: //invoke flightID query
+                            queryFlightByID();
+                            break;
+                        case 3: //invoke reservation
+                            makeReservation();
+                            break;
+                        case 4: //invoke reservation
+                            System.out.println("Exiting program...");
+                            break;
+                        default:
+                            System.out.println("Error: Please select a valid option.");
+                            break;
+                    }
+                } catch (Exception ex){
+                System.out.println("Error: Input is not an integer.");
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Client client;
+
         try {
             client = new Client(new DatagramSocket(), InetAddress.getByName("localhost"));
             System.out.println("Client is running ...");
-            client.sendMessage();
+//            client.sendMessage();
+            client.runConsole();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
 }
